ec331f957848536d60fc66ea5f28c5cf7d7631c1
Nilam Kumallah Anggreani <nilamanggreani23@gmail.com>
2026-05-08 01:04:15 +0700

    Update App-Kelas Bilingual.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -319,17 +319,6 @@ berikut:
           </ContentSection>
         );
 
-      case 'kelas-bilingual':
-        return (
-          <ContentSection title={"Kelas Bilingual"
-          }>
-            <div className="text-center py-12">
-              <User className="w-16 h-16 mx-auto mb-4 text-gray-400" />
-              <p className="text-gray-600">Konten akan segera hadir</p>
-            </div>
-          </ContentSection>
-        );
-
       case 'spiritual-learning':
         return (
           <ContentSection title={"Spiritual Learning"
